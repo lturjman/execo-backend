08a6f91f98116e77873cf58da0762adbbbd1c914
Laura Turjman <contact@lauraturjman.fr>
2025-06-29 14:36:04 +0200

    compute member share

--- routes/members.js
+++ routes/members.js
@@ -1,11 +1,32 @@
 var express = require("express");
 var router = express.Router({ mergeParams: true });
+const {
+  Types: { ObjectId },
+} = require("mongoose");
 
 require("../models/member");
 const Member = require("../models/member");
 const Group = require("../models/group");
 
-async function findGroup(req) {
+async function updateMembersShare(group) {
+  const members = await Member.find({ group: group._id });
+  const totalLeftoverQuery = await Member.aggregate([
+    { $match: { group: group._id } },
+    {
+      $group: {
+        _id: null,
+        totalLeftover: { $sum: "$leftover" },
+      },
+    },
+  ]);
+
+  members.forEach(async (member) => {
+    member.share = member.leftover / totalLeftoverQuery[0].totalLeftover;
+    await member.save();
+  });
+}
+
+async function findGroup(req, res) {
   const { groupId } = req.params;
 
   const group = await Group.findById(groupId);
@@ -15,7 +36,7 @@ async function findGroup(req) {
 }
 
 router.get("/", async (req, res) => {
-  const group = await findGroup(req);
+  const group = await findGroup(req, res);
 
   return Member.find({ group }).then((data) => {
     res.json({ data });
@@ -23,30 +44,41 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const group = await findGroup(req);
+  const group = await findGroup(req, res);
+  const leftover =
+    req.body.member.monthlyRevenue - req.body.member.monthlyCharges;
 
-  return Member.create({ ...req.body.member, group }).then((data) => {
-    res.json({ data });
-  });
+  return Member.create({ ...req.body.member, group, leftover }).then(
+    async (data) => {
+      await updateMembersShare(group);
+      res.json({ data });
+    }
+  );
 });
 
 router.put("/:id", async (req, res) => {
-  const group = await findGroup(req);
+  const group = await findGroup(req, res);
+  const leftover =
+    req.body.member.monthlyRevenue - req.body.member.monthlyCharges;
 
   return Member.findOneAndUpdate(
     { group, _id: req.params.id },
-    req.body.member
-  ).then((data) => {
+    { ...req.body.member, leftover }
+  ).then(async (data) => {
+    await updateMembersShare(group);
     res.json({ data });
   });
 });
 
 router.delete("/:id", async (req, res) => {
-  const group = await findGroup(req);
+  const group = await findGroup(req, res);
 
-  return Member.findOneAndDelete({ group, _id: req.params.id }).then((data) => {
-    res.json({ data });
-  });
+  return Member.findOneAndDelete({ group, _id: req.params.id }).then(
+    async (data) => {
+      await updateMembersShare(group);
+      res.json({ data });
+    }
+  );
 });
 
 module.exports = router;
