8e8d02b4a2cc6a116df4ea73698a24f83e08af49
Laura Turjman <contact@lauraturjman.fr>
2025-07-17 19:26:43 +0200

    add user to model group

--- models/group.js
+++ models/group.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const groupSchema = mongoose.Schema({
   name: { type: String, required: true },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Group = mongoose.model("Group", groupSchema);
