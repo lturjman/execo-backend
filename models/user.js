6421971fcb0e925d2a2682c8ab54f98f0bec4993
Laura Turjman <contact@lauraturjman.fr>
2026-08-26 18:17:59 +0200

    add timestamps on models

--- models/user.js
+++ models/user.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  monthlyRevenues: { type: Number, required: true },
-  monthlyCharges: { type: Number, required: true },
-  leftover: Number,
-});
+const userSchema = mongoose.Schema(
+  {
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    monthlyRevenues: { type: Number, required: true },
+    monthlyCharges: { type: Number, required: true },
+    leftover: Number,
+  },
+  { timestamps: true },
+);
 
 userSchema.pre("save", function (next) {
   this.leftover = (this.monthlyRevenues || 0) - (this.monthlyCharges || 0);
