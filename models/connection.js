4dfef191a3c23f1d8812ef74dfdcadd45c49f928
Laura Turjman <contact@lauraturjman.fr>
2026-09-21 16:37:05 +0200

    fix connexion timeout error

--- models/connection.js
+++ models/connection.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose')
 
 const connectionString = process.env.CONNECTION_STRING
 
-mongoose
-  .connect(connectionString, { connectTimeoutMS: 2000 })
-  .then(() => console.log('Database connected'))
-  .catch((error) => console.error(error))
+function connectDB () {
+  if (global.mongooseConnectionPromise) return global.mongooseConnectionPromise
+
+  global.mongooseConnectionPromise = mongoose.connect(connectionString, {
+    serverSelectionTimeoutMS: 6000,
+    connectTimeoutMS: 6000,
+    maxPoolSize: 10
+  })
+
+  global.mongooseConnectionPromise
+    .then(() => console.log('Database connected'))
+    .catch((error) => {
+      console.error('MongoDB connection failed:', error)
+      global.mongooseConnectionPromise = null
+    })
+
+  return global.mongooseConnectionPromise
+}
+
+connectDB()
+
+module.exports = connectDB
